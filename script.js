c2032367632ff69a8a228844c4e1003d891411f5
BorisChubiryaev <boriks7@yandex.ru>
2023-09-15 00:58:38 +0300

    updated buttons

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const input = document.querySelector(".todo-list__input"); // Получаем поле ввода
-  const itemList = document.querySelector(".todo-list__items"); // Получаем список задач
+  const input = document.querySelector(".todo-list__input");
+  const itemList = document.querySelector(".todo-list__items");
+  const addButton = document.querySelector(".todo-list__add-button");
+  const toggleButtonsButton = document.querySelector(
+    ".todo-list__toggle-buttons-button"
+  );
+  const toggleButtons = document.querySelector(".todo-list__toggle-buttons");
   const highlightButton = document.querySelector(
     ".todo-list__highlight-button"
-  ); // Получаем кнопку "Выделить чётные"
+  );
   const highlightOddButton = document.querySelector(
     ".todo-list__highlight-odd-button"
-  ); // Получаем кнопку "Выделить нечётные"
-  const addButton = document.querySelector(".todo-list__add-button"); // Получаем кнопку "Добавить задачу"
-  const deleteLastButton = document.querySelector(
-    ".todo-list__delete-last-button"
-  ); // Получаем кнопку "Удалить последний элемент"
+  );
   const deleteFirstButton = document.querySelector(
     ".todo-list__delete-first-button"
-  ); // Получаем кнопку "Удалить первый элемент"
+  );
+  const deleteLastButton = document.querySelector(
+    ".todo-list__delete-last-button"
+  );
+  const noTasksMessage = document.querySelector(".todo-list__no-tasks"); // Получаем элемент с надписью
 
   input.addEventListener("keyup", function (event) {
     if (event.key === "Enter" && input.value.trim() !== "") {
-      addItem(input.value); // Добавляем задачу при нажатии Enter, если поле не пустое
+      addItem(input.value);
       input.value = "";
     }
   });
 
   addButton.addEventListener("click", function () {
     if (input.value.trim() !== "") {
-      addItem(input.value); // Добавляем задачу при клике на кнопку, если поле не пустое
+      addItem(input.value);
       input.value = "";
+    }
+  });
+
+  toggleButtonsButton.addEventListener("click", function () {
+    // Переключаем видимость блока с кнопками
+    if (toggleButtons.style.display === "none") {
+      toggleButtons.style.display = "block";
+    } else {
+      toggleButtons.style.display = "none";
     }
   });
 
@@ -33,10 +47,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const items = document.querySelectorAll(".todo-list__item");
 
     items.forEach(function (item, index) {
-      if (index % 2 === 1) {
-        item.classList.add("todo-list__item--highlighted"); // Выделяем чётные элементы
+      if (index % 2 === 0) {
+        item.classList.add("todo-list__item--highlighted");
       } else {
-        item.classList.remove("todo-list__item--highlighted"); // Снимаем выделение с нечётных элементов
+        item.classList.remove("todo-list__item--highlighted");
       }
     });
   });
@@ -45,21 +59,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const items = document.querySelectorAll(".todo-list__item");
 
     items.forEach(function (item, index) {
-      if (index % 2 === 0) {
-        item.classList.add("todo-list__item--highlighted"); // Выделяем нечётные элементы
+      if (index % 2 === 1) {
+        item.classList.add("todo-list__item--highlighted");
       } else {
-        item.classList.remove("todo-list__item--highlighted"); // Снимаем выделение с чётных элементов
+        item.classList.remove("todo-list__item--highlighted");
       }
     });
-  });
-
-  deleteLastButton.addEventListener("click", function () {
-    const items = document.querySelectorAll(".todo-list__item");
-    const lastItem = items[items.length - 1];
-
-    if (lastItem) {
-      itemList.removeChild(lastItem); // Удаляем последний элемент, если он существует
-    }
   });
 
   deleteFirstButton.addEventListener("click", function () {
@@ -67,7 +72,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const firstItem = items[0];
 
     if (firstItem) {
-      itemList.removeChild(firstItem); // Удаляем первый элемент, если он существует
+      itemList.removeChild(firstItem);
+      checkTasks(); // Проверяем задачи после удаления
+    }
+  });
+
+  deleteLastButton.addEventListener("click", function () {
+    const items = document.querySelectorAll(".todo-list__item");
+    const lastItem = items[items.length - 1];
+
+    if (lastItem) {
+      itemList.removeChild(lastItem);
+      checkTasks(); // Проверяем задачи после удаления
     }
   });
 
@@ -81,30 +97,43 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const completeButton = document.createElement("button");
     completeButton.className = "todo-list__complete-button button";
-    completeButton.textContent = "Complete"; // Создаем кнопку "Complete" по умолчанию
+    completeButton.textContent = "Выполнена";
 
     const deleteButton = document.createElement("button");
     deleteButton.className = "todo-list__delete-button button";
     deleteButton.textContent = "Удалить";
 
     deleteButton.addEventListener("click", function () {
-      itemList.removeChild(listItem); // Удаляем задачу при нажатии кнопки "Удалить"
+      itemList.removeChild(listItem);
+      checkTasks(); // Проверяем задачи после удаления
     });
 
     completeButton.addEventListener("click", function () {
-      if (completeButton.textContent === "Complete") {
-        itemText.classList.add("todo-list__item-text--completed"); // Зачеркиваем текст задачи
-        completeButton.textContent = "Uncomplete"; // Меняем название кнопки на "Uncomplete"
-      } else {
-        itemText.classList.remove("todo-list__item-text--completed"); // Убираем зачеркивание текста
-        completeButton.textContent = "Complete"; // Меняем название кнопки на "Complete"
-      }
-      itemList.appendChild(listItem); // Перемещаем задачу в конец списка
-    });
+        if (completeButton.textContent === "Выполнена") {
+          itemText.classList.add("todo-list__item-text--completed");
+          completeButton.textContent = "Не выполнена";
+        } else {
+          itemText.classList.remove("todo-list__item-text--completed");
+          completeButton.textContent = "Выполнена";
+        }
+        itemList.appendChild(listItem);
+      });
+      
 
     listItem.appendChild(itemText);
     listItem.appendChild(completeButton);
     listItem.appendChild(deleteButton);
-    itemList.appendChild(listItem); // Добавляем задачу в список
+    itemList.appendChild(listItem);
+    checkTasks(); // Проверяем задачи после добавления
+  }
+
+  function checkTasks() {
+    const items = document.querySelectorAll(".todo-list__item");
+
+    if (items.length === 0) {
+      noTasksMessage.style.display = "block"; // Отображаем надпись, если нет задач
+    } else {
+      noTasksMessage.style.display = "none"; // Скрываем надпись, если есть задачи
+    }
   }
 });
